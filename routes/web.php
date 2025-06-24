6c01a4475b0b086ebf11be90061db7845cd0295d
frankamdev <fkamgang06@gmail.com>
2025-06-24 03:27:27 -1100

    route permission

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\Auth\RegisteredUserController;
+use App\Http\Controllers\PermissionController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+Route::resource('permission', PermissionController::class);
